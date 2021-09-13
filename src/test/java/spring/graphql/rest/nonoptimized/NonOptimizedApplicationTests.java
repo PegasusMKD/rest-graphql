c5cbaa867cbed456c9800297b1a64bb85a38fc3f
PegasusMKD <csprogrammers@outlook.com>
2021-09-13 05:34:03 +0200

    Reverting some stuff

--- src/test/java/spring/graphql/rest/nonoptimized/NonOptimizedApplicationTests.java
+++ src/test/java/spring/graphql/rest/nonoptimized/NonOptimizedApplicationTests.java
@@ -1,13 +1,29 @@
 package spring.graphql.rest.nonoptimized;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import spring.graphql.rest.nonoptimized.example.controller.AccountController;
+import spring.graphql.rest.nonoptimized.example.controller.AccountGraphQLController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class NonOptimizedApplicationTests {
 
+	@Autowired
+	private AccountController accountController;
+	@Autowired
+	private AccountGraphQLController graphQLController;
+
 	@Test
 	void contextLoads() {
+		assertThat(accountController).isNotNull();
+		assertThat(graphQLController).isNotNull();
+	}
+
+	void graphQLMetrics() {
+
 	}
 
 }

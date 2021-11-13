a019adccaf33f129f89bbb08c00d0f5aaae8c1d7
PegasusMKD <csprogrammers@outlook.com>
2021-11-13 09:45:03 +0100

    Experimented with customized MapStruct mappers for user Implemented @Component for filtering restricted properties
    Next up is implementing as AOP

--- src/main/java/spring/graphql/rest/rql/example/mappers/PersonMapper.java
+++ src/main/java/spring/graphql/rest/rql/example/mappers/PersonMapper.java
@@ -22,7 +22,7 @@ public abstract class PersonMapper {
 	public abstract Set<PersonDto> toPersonDtos(Set<Person> entity, @Context StringBuilder currentPath, @Context List<PropertyNode> propertyNodes, @Context List<String> properties, @Context String property);
 
 	@Named("dynamicPersons")
-	@Mapping(target = "account", expression = "java(properties.contains(\"account\") ? accountMapper.toAccountDto(entity.getAccount(), currentPath, propertyNodes, properties, \"account\") : null)")
+	@Mapping(target = "account", expression = "java(properties.contains(\"account\") ? accountMapper.rqlToAccountDto(entity.getAccount(), currentPath, propertyNodes, properties, \"account\") : null)")
 	public abstract PersonDto toPersonDto(Person entity, @Context StringBuilder currentPath, @Context List<PropertyNode> propertyNodes, @Context List<String> properties, @Context String property);
 
 }
--- src/main/java/spring/graphql/rest/rql/example/service/AccountService.java
+++ src/main/java/spring/graphql/rest/rql/example/service/AccountService.java
@@ -12,19 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 import spring.graphql.rest.rql.core.RQL;
 import spring.graphql.rest.rql.core.RQLAsyncRestriction;
 import spring.graphql.rest.rql.core.nodes.PropertyNode;
+import spring.graphql.rest.rql.core.restrict.RestrictRepository;
 import spring.graphql.rest.rql.example.controller.rest.PageRequestByExample;
 import spring.graphql.rest.rql.example.controller.rest.PageResponse;
 import spring.graphql.rest.rql.example.dto.AccountDto;
 import spring.graphql.rest.rql.example.dto.PersonDto;
 import spring.graphql.rest.rql.example.dto.querydsl.OptionalBooleanBuilder;
-import spring.graphql.rest.rql.example.mappers.AccountMapper;
+import spring.graphql.rest.rql.example.mappers.RealAccountMapper;
 import spring.graphql.rest.rql.example.models.Account;
 import spring.graphql.rest.rql.example.models.QAccount;
 import spring.graphql.rest.rql.example.models.QPerson;
 import spring.graphql.rest.rql.example.repository.AccountRepository;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,19 +37,22 @@ public class AccountService {
 	private final Logger logger = LoggerFactory.getLogger(AccountService.class);
 
 	private final AccountRepository accountRepository;
-	private final AccountMapper universalMapper;
+	private final RealAccountMapper accountMapper;
 	private final RQL rql;
+	private final RestrictRepository repository;
 
 	public AccountService(AccountRepository accountRepository,
-						  AccountMapper universalMapper, RQL rql) {
+						  RealAccountMapper accountMapper, RQL rql, RestrictRepository repository) {
 		this.accountRepository = accountRepository;
-		this.universalMapper = universalMapper;
+		this.accountMapper = accountMapper;
 		this.rql = rql;
+		this.repository = repository;
 	}
 
 	public AccountDto findOne(String id, String[] attributePaths) {
 		// Get minimal number of attributePaths for entity graph
 		List<PropertyNode> propertyNodes = createPropertyNodes(Account.class, attributePaths);
+		repository.filter(Account.class, attributePaths);
 
 		// Fetch data
 		Account entity = rql.rqlSingleSelect(
@@ -59,8 +62,7 @@ public class AccountService {
 		);
 
 		// Map properties
-		List<String> props = new ArrayList<>();
-		return universalMapper.toAccountDto(entity, new StringBuilder(), propertyNodes, props, "");
+		return accountMapper.toAccountDto(entity, propertyNodes);
 	}
 
 	@Transactional
@@ -82,9 +84,7 @@ public class AccountService {
 
 		// Map properties
 		long startTime = System.nanoTime();
-		List<String> props = new ArrayList<>();
-		List<AccountDto> content = new ArrayList<>(universalMapper.toAccountDtos(new HashSet<>(page.getContent()),
-				new StringBuilder(), propertyNodes, props, ""));
+		List<AccountDto> content = new ArrayList<>(accountMapper.toAccountDtos(page.getContent(), propertyNodes));
 		long endTime = System.nanoTime();
 //		logger.info("Mapping of paths took: {} ms -- Accounts", (endTime - startTime) / 1000000);
 

3c6c4b2147dcb65e4cee776199f4899b2c20a4f2
PegasusMKD <csprogrammers@outlook.com>
2023-04-01 01:58:35 +0200

    Create TPC-C Benchmark project

---
+++ tpcc/src/test/java/com/rql/tpcc/TpccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rql.tpcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
